c4bb28ab3e3c1f8eeca6fc4bc49346d89a04faea
aruun2aruun <arunkumar.selvaraj@westernacher.com>
2019-03-26 15:25:04 +0530

    fix

--- src/app/unauthorized/unauthorized.component.ts
+++ src/app/unauthorized/unauthorized.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {PageHeaderService} from '../core/services/page-header.service';
+import * as messageObject from '../message.json';
+import {MatDialog, MatDialogConfig, MatSnackBar} from '@angular/material';
+import {NotifyDialogComponent} from '../notify-dialog/notify-dialog.component';
+import {Router} from '@angular/router';
+import {AppraisalService} from '../core/services/appraisal.service';
 
 @Component({
   selector: 'app-unauthorized',
@@ -11,9 +16,45 @@ export class UnauthorizedComponent implements OnInit, OnDestroy {
   email: string;
   displayName: string;
 
-  constructor(private pageHeaderService: PageHeaderService) {
+  constructor(private router: Router,
+              private pageHeaderService: PageHeaderService,
+              public dialog: MatDialog,
+              private appraisalService: AppraisalService,
+              private snackBar: MatSnackBar) {
     this.pageHeaderService.setTitle('Unauthorized Access');
     this.pageHeaderService.hideCycle();
+  }
+
+  help() {
+    this.router.navigate(['/help']);
+  }
+
+
+
+  notify(result: any) {
+    this.appraisalService.notifyUser(result).subscribe(
+      response => {
+        this.snackBar.open(messageObject.NOTIFY.success, null, {
+          duration: 6000,
+        });
+      });
+  }
+
+  openSupportDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '50%';
+    dialogConfig.data = {
+      to: 'appraisal_westernacher@outlook.com',
+      subject: 'Subject',
+      body: 'Please specify your email Id while raising any question.'
+    };
+    const dialogRef = this.dialog.open(NotifyDialogComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.notify(result);
+      }
+    });
   }
 
   ngOnInit() {
