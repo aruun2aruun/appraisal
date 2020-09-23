341573fc6106437f412175467d96c0861187fd63
Arun Kumar Selvaraj <arunkumar.selvaraj@westernacher.com>
2020-09-23 20:12:38 +0530

    Fixes

--- src/app/appraisal/appraisal-summary/appraisal-summary.component.ts
+++ src/app/appraisal/appraisal-summary/appraisal-summary.component.ts
@@ -1,49 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { AppState } from 'src/app/app-state';
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-appraisal-summary',
-  templateUrl: './appraisal-summary.component.html',
-  styleUrls: ['./appraisal-summary.component.scss']
+  selector: "app-appraisal-summary",
+  templateUrl: "./appraisal-summary.component.html",
+  styleUrls: ["./appraisal-summary.component.scss"],
 })
-export class AppraisalSummaryComponent implements OnInit {
+export class AppraisalSummaryComponent implements OnChanges {
   @Input() appraisalReview: any;
+  @Input() appraisalCycle: any;
   roles: any[];
-  appraisalReviews: any[];
-  cycles: any[];
-  cycle: any;
-  today: any = Date.now();
   daysdiff: number;
 
-  constructor(
-    private store: Store<AppState>
-  ) { }
+  constructor() {}
 
-  ngOnInit() {
-    const roles$ = this.store.pipe(select('roles'));
-    roles$.subscribe(result => {
-      this.roles = result;
-    });
-    const appraisalReviews$ = this.store.pipe(select('appraisalReviews'));
-    appraisalReviews$.subscribe(result => {
-      this.appraisalReviews = result;
-    });
-    const cycles$ = this.store.pipe(select('cycles'));
-    cycles$.subscribe(result => {
-      this.cycles = result;
-      for (const cycle of result) {
-        if (cycle.status === 'ACTIVE') {
-          this.cycle = cycle;
-          const today = new Date(this.today).getTime();
-          const endDate = new Date(this.cycle.selfAppraisalDeadline).getTime();
-          if (today > endDate) {
-            this.daysdiff = 0;
-          } else {
-            this.daysdiff = Math.abs(endDate - today) / (1000 * 60 * 60 * 24);
-          }
-        }
-      }
-    });
+  ngOnChanges() {
+    const today = new Date().getTime();
+    const endDate = new Date(this.appraisalCycle.deadline.SELF_APPRAISAL).getTime();
+    if (today > endDate) {
+      this.daysdiff = 0;
+    } else {
+      this.daysdiff = Math.abs(endDate - today) / (1000 * 60 * 60 * 24);
+    }
   }
 }
--- src/app/appraisal/appraisal.component.ts
+++ src/app/appraisal/appraisal.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PageHeaderService } from '../core/services/page-header.service';
-import { AppraisalService } from '../core/services/appraisal.service';
-import { Store, select } from '@ngrx/store';
-import { AppState } from '../app-state';
-import { UserType } from '../model/user-type';
-import { AuthService } from '../core/services/auth.service';
-import { InitializationService } from '../core/services/initialization.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { PageHeaderService } from "../core/services/page-header.service";
+import { AppraisalService } from "../core/services/appraisal.service";
+import { Store, select } from "@ngrx/store";
+import { AppState } from "../app-state";
+import { UserType } from "../model/user-type";
+import { AuthService } from "../core/services/auth.service";
+import { InitializationService } from "../core/services/initialization.service";
 
 @Component({
-  selector: 'app-appraisal',
-  templateUrl: './appraisal.component.html',
-  styleUrls: ['./appraisal.component.scss'],
+  selector: "app-appraisal",
+  templateUrl: "./appraisal.component.html",
+  styleUrls: ["./appraisal.component.scss"],
 })
 export class AppraisalComponent implements OnInit {
   queryParamId: any;
@@ -32,7 +32,7 @@ export class AppraisalComponent implements OnInit {
     public initializationService: InitializationService,
     private store: Store<AppState>
   ) {
-    this.pageHeaderService.setTitle('Appraisal');
+    this.pageHeaderService.setTitle("Appraisal");
   }
 
   ngOnInit() {
@@ -48,7 +48,7 @@ export class AppraisalComponent implements OnInit {
 
   initialize() {
     this.initializationService.loggedInUser$.subscribe((loggedInUser) => {
-      console.log('****loggedInUser');
+      console.log("****loggedInUser");
       if (loggedInUser) {
         this.store
           .pipe(
@@ -63,17 +63,19 @@ export class AppraisalComponent implements OnInit {
             )
           )
           .subscribe((appraisalReview) => {
-            console.log('****appraisal');
+            console.log("****appraisal");
             this.appraisalReview = appraisalReview;
             if (appraisalReview) {
               this.store
                 .pipe(
                   select((state) =>
-                    state.users.find((item) => item.id === appraisalReview.employeeId)
+                    state.users.find(
+                      (item) => item.id === appraisalReview.employeeId
+                    )
                   )
                 )
                 .subscribe((user) => {
-                  console.log('****user');
+                  console.log("****user");
                   this.store
                     .pipe(
                       select((state) =>
@@ -85,20 +87,28 @@ export class AppraisalComponent implements OnInit {
                     .subscribe((goals) => {
                       this.jobGoals = goals.filter((item) => item.job !== null);
                       this.jobGoalsGroup = this.jobGoals
-                                            .map(item => item.group)
-                                            .filter((value, index, self) => self.indexOf(value) === index);
+                        .map((item) => item.group)
+                        .filter(
+                          (value, index, self) => self.indexOf(value) === index
+                        );
                       this.cuGoals = goals.filter((item) => item.cu !== null);
                     });
                 });
-                this.getAppraisalCycle(appraisalReview);
-                this.getReviewGoal(appraisalReview);
+              this.getAppraisalCycle(appraisalReview);
+              this.getReviewGoal(appraisalReview);
             }
           });
 
-          const roles$ = this.store.pipe(select('roles'));
-          roles$.subscribe(result => {
-            const test = result.filter((item) => (item.reviewerId === loggedInUser.id) && (item.employeeId === loggedInUser.id))[0];
-            console.log(test);
+        this.store
+          .pipe(
+            select((state) =>
+              state.roles.filter(
+                (item) => item.employeeId === this.appraisalReview.employeeId
+              )
+            )
+          )
+          .subscribe((result) => {
+            this.roles = result;
           });
       }
     });
@@ -116,22 +126,22 @@ export class AppraisalComponent implements OnInit {
   }
 
   getAppraisalCycle(appraisalReview) {
-      this.store
-        .pipe(
-          select((state) =>
-            state.cycles.find(
-              (item) => item.id === appraisalReview.cycleId
-            )
-          )
+    this.store
+      .pipe(
+        select((state) =>
+          state.cycles.find((item) => item.id === appraisalReview.cycleId)
         )
-        .subscribe((appraisalCycle) => {
-          this.appraisalCycle = appraisalCycle;
-        });
+      )
+      .subscribe((appraisalCycle) => {
+        this.appraisalCycle = appraisalCycle;
+      });
   }
 
-  changeGoalReview (event) {
+  changeGoalReview(event) {
     console.log(event);
-    const appraisalGoal = this.appraisalGoals.find((item) => item.id === event.id);
+    const appraisalGoal = this.appraisalGoals.find(
+      (item) => item.id === event.id
+    );
     console.log(appraisalGoal);
     appraisalGoal.comment = event.comment;
     appraisalGoal.rating = event.rating;
@@ -139,29 +149,41 @@ export class AppraisalComponent implements OnInit {
 
   saveAsDraft() {
     this.initializationService.loggedInUser$.subscribe((loggedInUser) => {
-      this.appraisalService.saveReviewGoal(this.appraisalGoals.filter((item) => item.reviewerId === loggedInUser.id)).subscribe(
-        (response) => {
-          console.log(response);
-          this.getReviewGoal(this.appraisalReview);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      this.appraisalService
+        .saveReviewGoal(
+          this.appraisalGoals.filter(
+            (item) => item.reviewerId === loggedInUser.id
+          )
+        )
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.getReviewGoal(this.appraisalReview);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     });
   }
 
-  submitAppraisal () {
+  submitAppraisal() {
     this.saveAsDraft();
     this.initializationService.loggedInUser$.subscribe((loggedInUser) => {
-      this.appraisalService.submitReviewGoal(this.appraisalGoals.filter((item) => item.reviewerId === loggedInUser.id)).subscribe(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      this.appraisalService
+        .submitReviewGoal(
+          this.appraisalGoals.filter(
+            (item) => item.reviewerId === loggedInUser.id
+          )
+        )
+        .subscribe(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     });
   }
 }
