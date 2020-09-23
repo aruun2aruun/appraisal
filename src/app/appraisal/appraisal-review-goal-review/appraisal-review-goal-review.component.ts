258de0ff97c329368d22902f29db2dfecc1d9c30
Arun Kumar Selvaraj <arunkumar.selvaraj@westernacher.com>
2020-09-23 19:30:32 +0530

    Fixes

--- src/app/appraisal/appraisal-review-goal-review/appraisal-review-goal-review.component.ts
+++ src/app/appraisal/appraisal-review-goal-review/appraisal-review-goal-review.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
-import { InitializationService } from 'src/app/core/services/initialization.service';
-import { Store, select } from '@ngrx/store';
-import { AppState } from 'src/app/app-state';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+} from "@angular/core";
+import { InitializationService } from "src/app/core/services/initialization.service";
+import { Store, select } from "@ngrx/store";
+import { AppState } from "src/app/app-state";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-appraisal-review-goal-review',
-  templateUrl: './appraisal-review-goal-review.component.html',
-  styleUrls: ['./appraisal-review-goal-review.component.scss'],
+  selector: "app-appraisal-review-goal-review",
+  templateUrl: "./appraisal-review-goal-review.component.html",
+  styleUrls: ["./appraisal-review-goal-review.component.scss"],
 })
 export class AppraisalReviewGoalReviewComponent implements OnChanges {
   @Input() jobGoal: any;
   @Input() appraisalGoals: any;
   @Input() appraisalReview: any;
   ratings: string[] = [
-    '1 - Deficient',
-    '2 - Improvements Required',
-    '3 - Meets Expectations',
-    '4 - Above Expectations',
-    '5 - Excellent',
+    "1 - Deficient",
+    "2 - Improvements Required",
+    "3 - Meets Expectations",
+    "4 - Above Expectations",
+    "5 - Excellent",
   ];
 
   @Output() testValue = new EventEmitter();
 
   filteredGoals: any = [];
+  appraisalCycle: any;
 
   constructor(
     private initializationService: InitializationService,
@@ -49,32 +57,76 @@ export class AppraisalReviewGoalReviewComponent implements OnChanges {
           };
         });
     }
+    if (this.appraisalReview) {
+      this.store
+        .pipe(
+          select((state) =>
+            state.cycles.find(
+              (item) => item.id === this.appraisalReview.cycleId
+            )
+          )
+        )
+        .subscribe((appraisalCycle) => {
+          this.appraisalCycle = appraisalCycle;
+        });
+    }
   }
 
   getVisibility(loggedInUser, appraisalGoal) {
-    let visibility = 'WRITE';
-    if (loggedInUser.id !== appraisalGoal.reviewerId || appraisalGoal.reviewerType !== this.appraisalReview.status || appraisalGoal.isComplete) {
-      visibility = 'READ';
+    let visibility = "WRITE";
+    if (
+      loggedInUser.id !== appraisalGoal.reviewerId ||
+      appraisalGoal.reviewerType !== this.appraisalReview.status ||
+      appraisalGoal.isComplete
+    ) {
+      visibility = "READ";
     }
 
     switch (this.appraisalReview.status) {
-      case 'SELF_APPRAISAL':
-        if (['PROJECT_MANAGER', 'REPORTING_MANAGER', 'PRACTICE_DIRECTOR', 'HR'].includes(appraisalGoal.reviewerType)) {
-          visibility = 'HIDE';
+      case "SELF_APPRAISAL":
+        if (
+          [
+            "PROJECT_MANAGER",
+            "REPORTING_MANAGER",
+            "PRACTICE_DIRECTOR",
+            "HR",
+          ].includes(appraisalGoal.reviewerType)
+        ) {
+          visibility = "HIDE";
         }
-        if (appraisalGoal.reviewerType === 'Self' && !appraisalGoal.isComplete && appraisalGoal.reviewerId !== loggedInUser.id) {
-          visibility = 'HIDE';
+        if (
+          appraisalGoal.reviewerType === "Self" &&
+          !appraisalGoal.isComplete &&
+          appraisalGoal.reviewerId !== loggedInUser.id
+        ) {
+          visibility = "HIDE";
         }
         break;
-      case 'PROJECT_MANAGER':
-        if (['REPORTING_MANAGER', 'PRACTICE_DIRECTOR', 'HR'].includes(appraisalGoal.reviewerType)) {
-          visibility = 'HIDE';
+      case "PROJECT_MANAGER":
+        if (
+          ["REPORTING_MANAGER", "PRACTICE_DIRECTOR", "HR"].includes(
+            appraisalGoal.reviewerType
+          )
+        ) {
+          visibility = "HIDE";
         }
-        if (appraisalGoal.reviewerType === 'PROJECT_MANAGER' && !appraisalGoal.isComplete && appraisalGoal.reviewerId !== loggedInUser.id) {
-          visibility = 'HIDE';
+        if (
+          appraisalGoal.reviewerType === "PROJECT_MANAGER" &&
+          !appraisalGoal.isComplete &&
+          appraisalGoal.reviewerId !== loggedInUser.id
+        ) {
+          visibility = "HIDE";
         }
         break;
       // TBD Extend for other stages using getVisibilityBasedOnStatusAndType()
+    }
+
+    if (
+      this.appraisalCycle.showReviewToSelf &&
+      loggedInUser.id === appraisalGoal.employeeId &&
+      loggedInUser.id !== appraisalGoal.reviewerId
+    ) {
+      visibility = "HIDE";
     }
 
     return visibility;
@@ -84,7 +136,7 @@ export class AppraisalReviewGoalReviewComponent implements OnChanges {
     // TBD reusable function
   }
 
-  onChange (id, comment, rating) {
-    this.testValue.emit({id, comment, rating});
+  onChange(id, comment, rating) {
+    this.testValue.emit({ id, comment, rating });
   }
 }
