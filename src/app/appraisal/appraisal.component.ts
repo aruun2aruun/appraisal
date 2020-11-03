42b88450b8e76c9a84bceb1c9646ff4b12aeb029
Arun Kumar Selvaraj <arunkumar.selvaraj@westernacher.com>
2020-11-03 18:49:14 +0530

    fixes

--- src/app/appraisal/appraisal.component.ts
+++ src/app/appraisal/appraisal.component.ts
@@ -180,6 +180,7 @@ export class AppraisalComponent implements OnInit {
   }
 
   submitAppraisal() {
+    this.saveAsDraft();
     this.initializationService.loggedInUser$.subscribe((loggedInUser) => {
       const submitObj = this.appraisalGoals.filter(item => (item.reviewerId === loggedInUser.id &&
                                                        item.reviewerType === this.appraisalReview.status));
