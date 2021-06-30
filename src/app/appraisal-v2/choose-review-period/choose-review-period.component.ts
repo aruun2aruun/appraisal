4573c1ddb783c754befc66b24e2a4a228e52009b
manojvictor <fmanojvictor@gmail.com>
2021-06-30 19:21:36 +0530

    fixes for employeeid

--- src/app/appraisal-v2/choose-review-period/choose-review-period.component.ts
+++ src/app/appraisal-v2/choose-review-period/choose-review-period.component.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { PageHeaderService } from "src/app/core/services/page-header.service";
 import { UserService } from "src/app/core/services/user.service";
 import { UserType } from "src/app/model/user-type";
@@ -17,17 +17,22 @@ export class ChooseReviewPeriodComponent implements OnInit {
   selectedReviewPeriod: any;
   from: any = null;
   to: any = null;
+  employeeId: string;
 
   constructor(
     private pageHeaderService: PageHeaderService,
     private userService: UserService,
     private router: Router,
+    private route: ActivatedRoute,
     private datePipe: DatePipe
   ) {
     this.pageHeaderService.setTitle("Choose Review Period");
   }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.employeeId = params.employeeId;
+    });
     setTimeout(() => {
       this.userService.getLoggedInUserData().subscribe((data) => {
         this.loggedInUser = data;
@@ -37,7 +42,7 @@ export class ChooseReviewPeriodComponent implements OnInit {
 
   reviewPeriodSelection() {
     if (this.selectedReviewPeriod === "predefined") {
-      this.router.navigate([`appraisalv2/${this.loggedInUser.id}`], {
+      this.router.navigate([`appraisalv2/${this.employeeId}`], {
         queryParams: {
           cycleId: "1234",
         },
@@ -47,7 +52,7 @@ export class ChooseReviewPeriodComponent implements OnInit {
       this.from !== null &&
       this.to !== null
     ) {
-      this.router.navigate([`appraisalv2/${this.loggedInUser.id}`], {
+      this.router.navigate([`appraisalv2/${this.employeeId}`], {
         queryParams: {
           from: this.datePipe.transform(this.from, "yyyyMM"),
           to: this.datePipe.transform(this.to, "yyyyMM"),
